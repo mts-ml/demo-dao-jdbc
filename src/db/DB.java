315925b58ed5a01b5dc5afb45f72c0ff311fb0cd
Mateus Lima <mtsmlima@gmail.com>
2025-12-18 13:24:06 -0300

    Initial commit

---
+++ src/db/DB.java
@@ -0,0 +1,39 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                connection = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+
+        return connection;
+    }
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+
+            props.load(fs);
+
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+}
