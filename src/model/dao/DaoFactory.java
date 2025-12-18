84ab723ed5b221ca8420a3415019fe547db7c16c
Mateus Lima <mtsmlima@gmail.com>
2025-12-18 15:56:28 -0300

    Add two new classes

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package model.dao;
+
+import model.dao.implementation.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC();
+    }
+}
