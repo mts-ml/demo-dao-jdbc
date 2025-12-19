f81ea989eb6e2d0ee4245db5de1a80cb7849f923
Mateus Lima <mtsmlima@gmail.com>
2025-12-19 13:08:33 -0300

    test(Program): manual test for SellerDao.findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,13 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-        System.out.println(department);
-
+        System.out.println("Test 1:");
         SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
