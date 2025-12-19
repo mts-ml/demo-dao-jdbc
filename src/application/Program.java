589d7db4286bae968bf3c4da0b9bed6bd2859267
Mateus Lima <mtsmlima@gmail.com>
2025-12-19 15:22:50 -0300

    test(Program): new manual test with findByDepartment()

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,15 +2,25 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Program {
 
     public static void main(String[] args) {
-
-        System.out.println("Test 1:");
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+
+        System.out.println("Test 1: seller findById");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        System.out.println();
+
+        System.out.println("Teste 2: seller findByDepartment");
+        List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+        list.forEach(System.out::println);
+        System.out.println();
     }
 }
